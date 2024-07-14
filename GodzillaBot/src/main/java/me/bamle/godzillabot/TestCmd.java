f16530ccaa2863ce98734b52e45a410f16848a63
Håkon <haakon.siegwarth@gmail.com>
2024-07-14 19:02:26 +0200

    Added first version of the command postmsg

--- GodzillaBot/src/main/java/me/bamle/godzillabot/TestCmd.java
+++ GodzillaBot/src/main/java/me/bamle/godzillabot/TestCmd.java
@@ -1,8 +1,12 @@
 package me.bamle.godzillabot;
 
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
@@ -18,8 +22,14 @@ public class TestCmd extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         String command = event.getName();
-        if (command.equalsIgnoreCase("test")) {
-            event.reply("Test").queue();
+
+        if (command.equalsIgnoreCase("postmsg")) {
+            TextChannel kanal = event.getOption("kanal").getAsChannel().asTextChannel();
+            String msg = event.getOption("melding").getAsString();
+            kanal.sendMessage(msg).queue();
+            event.reply("Sendte melding ```" + msg + "``` i " + kanal.getAsMention()).queue();
+
+
 
         }
 
@@ -28,7 +38,7 @@ public class TestCmd extends ListenerAdapter {
 
     @Override
     public void onGuildReady(GuildReadyEvent event) {
-        commandData.add(Commands.slash("test", "test kommando"));
+        commandData.add(Commands.slash("postmsg", "Send en melding i en kanal").addOption(OptionType.CHANNEL, "kanal","Hvilken kanal ønsker du?").addOption(OptionType.STRING, "melding", "Innhold i melding"));
         event.getJDA().updateCommands().addCommands(commandData).queue();
         commandData.clear();
     }
